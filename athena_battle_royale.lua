95727526edc47701f33af3c457d2fd1b5206fdc1
coastss <55933271+coastss@users.noreply.github.com>
2022-01-22 23:24:49 -0500

    Create athena_battle_royale.lua

---
+++ athena_battle_royale.lua
@@ -0,0 +1,127 @@
+local UserInputService = game:GetService("UserInputService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local EquippedWeapon = LocalPlayer.Character:WaitForChild("Equipped")
+local ItemScript = LocalPlayer:WaitForChild("PlayerScripts"):WaitForChild("ItemScript")
+local MouseModule = require(ReplicatedStorage:WaitForChild("Modules"):WaitForChild("Mouse"))
+
+local Events = ReplicatedStorage:WaitForChild("Events")
+local RecoilEvent = Events:WaitForChild("Recoil")
+local ShakeEvent = Events:WaitForChild("Shake")
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/coastss/universal/main/utilities/ui_library.lua"))()
+
+local function GetClosestPlayer()
+    if not LocalPlayer.Character then return end
+
+    local ClosestPlayer = nil
+    local FarthestDistance = math.huge
+
+    for Index, Player in pairs(Players:GetPlayers()) do
+        if Player ~= LocalPlayer and (Player.Character and Player.Character:FindFirstChildWhichIsA("Humanoid").Health > 0)then
+            local HumanoidRootPart = Player.Character:FindFirstChild("HumanoidRootPart")
+
+            if HumanoidRootPart then
+                local Distance = nil
+
+                if Library.flags["Silent Aim Method"] == "Mouse" then
+                    local ScreenPosition, OnScreen = workspace.CurrentCamera:WorldToViewportPoint(HumanoidRootPart.Position)
+
+                    if OnScreen then
+                        Distance = (Vector2.new(ScreenPosition.X, ScreenPosition.Y) - UserInputService:GetMouseLocation()).Magnitude
+                    end
+                elseif Library.flags["Silent Aim Method"] == "Character" then
+                    Distance = LocalPlayer:DistanceFromCharacter(HumanoidRootPart.Position)
+                end
+                        
+                if Distance and (Distance < FarthestDistance) then
+                    FarthestDistance = Distance
+                    ClosestPlayer = Player
+                end
+            end
+        end
+    end
+
+    return ClosestPlayer
+end
+
+--// Silent aim
+local OldRayNew
+OldRayNew = hookfunction(Ray.new, function(...)
+    local Arguments = {...}
+    
+    if getfenv(2).script == ItemScript and Library.flags["Silent Aim Enabled"] then
+        local ClosestPlayer = GetClosestPlayer()
+        local WeaponConfig = require(EquippedWeapon.Value.Config)
+
+        if (ClosestPlayer and ClosestPlayer.Character) and WeaponConfig then
+            local HitPart = ClosestPlayer.Character:FindFirstChild("HumanoidRootPart")
+
+            if math.random(Library.flags["Silent Aim Headshot Chance"], 100) == 100 then
+                HitPart = ClosestPlayer.Character:FindFirstChild("Head")
+            end
+
+            if HitPart then
+                Arguments[2] = (CFrame.new(EquippedWeapon.Value.Handle.Muzzle.WorldPosition, HitPart.Position).LookVector * WeaponConfig.Range)
+            end
+        end
+    end
+    
+    return OldRayNew(unpack(Arguments))
+end)
+
+--// Weapon mods
+local OldNameCall = nil
+OldNameCall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+    local NamecallMethod = getnamecallmethod()
+    local Arguments = {...}
+    
+    if NamecallMethod == "Fire" then
+        if self == RecoilEvent and Library.flags["No Recoil"] then
+            Arguments[1] = Vector3.new()
+        elseif self == ShakeEvent and Library.flags["No Shake"] then
+            Arguments[1] = 0
+        end
+    end
+
+    if NamecallMethod == "FireServer" and self.Name == "Hit" then
+        print(self, ...)
+    end
+    
+    return OldNameCall(self, unpack(Arguments))
+end))
+
+
+--// Library
+local AthenaBRTab = Library:CreateWindow("Athena: BR!")
+
+local SilentAim = AthenaBRTab:AddFolder("Silent Aim")
+SilentAim:AddToggle({text = "Enabled", flag = "Silent Aim Enabled"})
+SilentAim:AddSlider({
+    text = "Headshot Chance %",
+    flag = "Silent Aim Headshot Chance",
+    min = 0,
+    max = 100,
+    float = 1
+})
+
+SilentAim:AddList({
+    text = "Closest To",
+    flag = "Silent Aim Method",
+    values = {"Mouse", "Character"}
+})
+
+local WeaponMods = AthenaBRTab:AddFolder("Weapon Mods")
+WeaponMods:AddToggle({text = "No Recoil", flag = "No Recoil"})
+WeaponMods:AddToggle({text = "No Shake", flag = "No Shake"})
+
+Library:Init()
+UserInputService.InputBegan:Connect(function(Input, GameProcessedEvent)
+    if GameProcessedEvent then return end
+    if Input.KeyCode == Enum.KeyCode.RightControl then
+        Library:Close()
+    end
+end)
